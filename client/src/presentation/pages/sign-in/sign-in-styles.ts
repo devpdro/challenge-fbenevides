633a47088eaa8a75575bbf1321ed06c08657ebb0
Victor <victorh.pedr@gmail.com>
2024-09-16 17:33:32 -0300

    feat: login page finalized and form implemented with validation and error handling

--- client/src/presentation/pages/sign-in/sign-in-styles.ts
+++ client/src/presentation/pages/sign-in/sign-in-styles.ts
@@ -1,3 +1,75 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  background-color: ${({ theme }) => theme.colors.silver};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 100vh;
+`;
+export const Form = styled.form`
+  background-color: ${({ theme }) => theme.colors.background};
+
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  max-width: 450px;
+  padding: 28px;
+  gap: 10px;
+  border-radius: 20px;
+`;
+
+export const Title = styled.h1`
+  font-size: ${({ theme }) => theme.fontSizes.extraLarge};
+  color: ${({ theme }) => theme.colors.info};
+  font-weight: ${({ theme }) => theme.fontWeights.default};
+
+  letter-spacing: -1px;
+  line-height: 2.075rem;
+`;
+export const Subtitle = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.default};
+  color: ${({ theme }) => theme.colors.info};
+  word-wrap: break-word;
+
+  line-height: 1.375rem;
+`;
+
+export const Input = styled.input`
+  font-size: ${({ theme }) => theme.fontSizes.default};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.info};
+
+  width: 100%;
+  margin: 10px 0;
+  padding: 10px;
+  letter-spacing: -1px;
+  border-radius: 10px;
+`;
+export const Signup = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.default};
+  color: ${({ theme }) => theme.colors.info};
+  text-align: center;
+
+  margin-top: 2px;
+  line-height: 1.375rem;
+
+  a {
+    color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: underline;
+  }
+`;
+
+export const Error = styled.span`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSizes.small};
+    font-weight: ${theme.fontWeights.default};
+    color: ${theme.colors.error};
+    text-align: center;
+
+    margin-left: 5px;
+    width: 100%;
+  `}
+`;
--- client/src/presentation/pages/sign-in/sign-in.tsx
+++ client/src/presentation/pages/sign-in/sign-in.tsx
@@ -1,10 +1,91 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { Grid } from "@mantine/core";
+
+import { Button } from "src/presentation/components";
+import { BASE_URL_API } from "src/constants";
+import { signInValidationSchema } from "src/utils";
+
 import * as S from "./sign-in-styles";
 
+type SignInFormSchema = yup.InferType<typeof signInValidationSchema>;
+
 const SignIn = () => {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormSchema>({
+    resolver: yupResolver(signInValidationSchema),
+  });
+
+  const onSubmit: SubmitHandler<SignInFormSchema> = async (data) => {
+    try {
+      const response = await fetch(`${BASE_URL_API}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao fazer login. Por favor, tente novamente...");
+      }
+
+      navigate("/home");
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage("Erro no servidor. Tente novamente mais tarde...");
+      }
+    }
+  };
+
   return (
     <S.Container>
-      <h1>Entrar</h1>
+      <S.Form onSubmit={handleSubmit(onSubmit)}>
+        <S.Title>Iniciar sessão na sua conta</S.Title>
+        <S.Subtitle>
+          Comece a utilizar a nossa aplicação, basta entrar na sua conta e
+          desfrutar da experiência.
+        </S.Subtitle>
+        <Grid>
+          <Grid.Col span={{ md: 12, lg: 12 }}>
+            <S.Input {...register("email")} type="email" placeholder="Email" />
+            {errors.email && <S.Error>{errors.email.message}</S.Error>}
+          </Grid.Col>
+          <Grid.Col span={{ md: 12, lg: 12 }}>
+            <S.Input
+              {...register("password")}
+              type="password"
+              placeholder="Senha"
+            />
+            {errors.password && <S.Error>{errors.password.message}</S.Error>}
+          </Grid.Col>
+        </Grid>
+
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "ENTRANDO..." : "ENTRAR"}
+        </Button>
+
+        <S.Signup>
+          Não tem conta? <a href="/registrar">Criar conta</a>
+        </S.Signup>
+
+        {errorMessage && <S.Error>{errorMessage}</S.Error>}
+      </S.Form>
     </S.Container>
   );
 };
+
 export default SignIn;
